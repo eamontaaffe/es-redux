cb230bcfd34d8f825f98e14ee4cefb54d203a228
Eamon Taaffe <eamontaaffe@gmail.com>
2017-05-24 22:27:33 +1000

    got a basic webpack, jest, babel setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
