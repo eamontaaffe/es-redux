3a7de47f9e6f9a91b94ca222bff90711aa36085a
Eamon Taaffe <eamontaaffe@gmail.com>
2017-05-24 23:04:57 +1000

    started scaffolding out some basic stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,69 @@
-export default (x, y) => (x + y);
+/**
+ * Actions
+ */
+
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+function incrementCounter() {
+  return {
+    type: INCREMENT_COUNTER,
+  }
+}
+
+function decrementCounter() {
+  return {
+    type: DECREMENT_COUNTER,
+  }
+}
+
+/**
+ * Store
+ */
+
+function createStore(preloadedState = []) {
+
+  /**
+   * The current state is an array rather than a object. We know that is must only
+   * ever have events appended.
+   */
+  currentState = preloadedState;
+
+  function getState() {
+    return currentState;
+  }
+
+  /**
+   * Append the event to the state, notify any subscribers of the event.
+   * 
+   * Also do some checking stuff like type
+   */
+  function dispatch(action) {
+
+  }
+
+  return {
+    dispatch,
+    getState,
+  }
+}
+
+
+/**
+ * Reducers
+ */
+
+function counter(state, action) {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return {
+        counter: state.counter + 1,
+      };
+    case DECREMENT_COUNTER:
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
